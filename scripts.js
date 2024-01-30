a92960b18ee8c6a4263108e02864e03561ade4ec
Jeremiah Samuel <livinalt@gmail.com>
2024-01-30 15:01:14 +0100

    update

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu_bar');
+    const nav_Links = document.getElementById('navLinks');
+
+    function toggleNavLinks() {
+
+        if (menuIcon.style.display === 'inline') {
+
+            nav_Links.style.display = 'none'; 
+        } 
+        
+        else {
+            nav_Links.style.display = 'inline';
+        }
+    }
+
+    menuIcon.addEventListener('click', toggleNavLinks);
+});
